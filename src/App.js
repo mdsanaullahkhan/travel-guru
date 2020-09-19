b5429825304e2f1e31ef0c61bd384eaf1971f00c
mdsanaullahkhan <advsamikhandu@gmail.com>
2020-09-20 00:16:24 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import LoginHeader from './components/loginHeader/LoginHeader';
+import Login from './components/Login/Login';
+import FbAndGmailSignIn from './components/Login/FbAndGmailSignIn';
+import Register from './components/Register/Register';
+import NotFound from './components/notFound/NotFound';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import Destination from './components/Destination/Destination';
 
+export const UserContext = createContext();
 function App() {
+  const [loggedInUser,setLoggedInUser] = useState({});
+  const [city,setCity] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser,setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <LoginHeader></LoginHeader>
+            <Login></Login>
+            <FbAndGmailSignIn></FbAndGmailSignIn>
+          </Route>
+          <Route path="/register">
+            <LoginHeader></LoginHeader>
+            <Register></Register>
+            <FbAndGmailSignIn></FbAndGmailSignIn>
+          </Route>
+          <PrivateRoute path="/destination">
+            <Destination city={city}></Destination>
+          </PrivateRoute>
+          <Route exact path="/">
+            <Header></Header>
+            <Home setCity={setCity}></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQrWZfESAk5oF2GDdA7wY8JiGdvdbkFKo",
+  authDomain: "travel-guru-e35a6.firebaseapp.com",
+  databaseURL: "https://travel-guru-e35a6.firebaseio.com",
+  projectId: "travel-guru-e35a6",
+  storageBucket: "travel-guru-e35a6.appspot.com",
+  messagingSenderId: "256714345646",
+  appId: "1:256714345646:web:c93faae9aacee3c95c313f"
+};
+
+  export default firebaseConfig;
